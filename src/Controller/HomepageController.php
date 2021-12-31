be5ce76eddab2fbfdcbdb4f811ade0def3bd58da
Tito Miguel Costa <titomiguelcosta@gmail.com>
2021-12-31 21:45:02 +0100

    added entity

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Request;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function __construct(private EntityManagerInterface $entityManager)
     {
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(HttpFoundationRequest $request): Response
+    {
+        $track = new Request();
+        $track->setIp($request->getClientIp());
+        $track->setCreatedAt(new DateTimeImmutable());
+
+        $this->entityManager->persist($track);
+        $this->entityManager->flush();
+
         return $this->json([
             'message' => 'Wish me luck!!!',
         ]);
---
+++ src/Entity/Request.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RequestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RequestRepository::class)]
+class Request
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $ip;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function setIp(?string $ip): self
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
